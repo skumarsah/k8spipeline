b15270671ebaee74c13bdc5e15baed6ea494e13d
santosh1432007@hotmail.com <Hariom1>
2020-05-19 23:36:28 +0200

    Links Fixed

--- app/server.js
+++ app/server.js
@@ -6,11 +6,13 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(app.router);
 app.get('/', (req, res) => {
   res.send('Congratulations! The Pipeline is Ready');
 });
 app.get('/nodejs', (req, res) => {
-  res.redirect('index.html')
+  res.redirect('https://www.magicsqaure.games')
 });
 
 app.listen(PORT, HOST);
