63551747093f19637c780fddb9013633de1a17f7
santosh1432007@hotmail.com <Hariom1>
2020-05-20 00:07:38 +0200

    Moved file

--- app/server.js
+++ app/server.js
@@ -6,10 +6,9 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
-app.use(express.static(__dirname + "/public"));
-app.use(app.router);
 app.get('/', (req, res) => {
-  res.send('Congratulations! The Pipeline is Ready');
+  res.sendfile('index.html');
 });
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
